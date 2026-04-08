2c37018656f2ef55743eb4b991b8274fb2cf5b1a
Nathan Selvidge <nselvidge@gmail.com>
2026-04-08 11:48:05 -0700

    feat: Include runtime when listing sandbox evals (#1764)
    In order to add support for python based sandboxes we need to pass the
    runtime into the list request. The change is backwards compatible and
    the value will be stripped on out of date data plane versions.

--- js/src/sandbox.ts
+++ js/src/sandbox.ts
@@ -120,6 +120,7 @@ export async function registerSandbox(
         provider: options.sandbox.provider,
         snapshot_ref: options.sandbox.snapshotRef,
       },
+      runtime_context: runtimeContext,
       entrypoints: options.entrypoints,
       project_id: projectId,
     },
