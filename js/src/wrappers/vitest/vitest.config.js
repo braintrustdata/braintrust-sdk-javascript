406a4b5c400de815d0bd543eec7d393756af6fea
Luca Forstner <luca.forstner@gmail.com>
2026-05-04 20:13:05 +0200

    ci: Set reporters to default (#1937)
    I don't like that vitest is spamming ci summaries

--- js/src/wrappers/vitest/vitest.config.js
+++ js/src/wrappers/vitest/vitest.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     reporters: ["default"],
-    include: ["tests/**/*.test.ts", "src/**/*.test.ts"],
   },
 });
