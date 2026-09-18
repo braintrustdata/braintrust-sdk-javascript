fada38b9c9ca1574031528fec54d15e7eac5a833
Aaron <aaron.ersoz@braintrustdata.com>
2026-09-18 05:47:47 -0700

    fix(pi-sdk): Fix nesting of child spans in tool spans (#2499)

---
+++ .changeset/pi-tool-current-span.md
@@ -0,0 +1,5 @@
+---
+"braintrust": patch
+---
+
+Keep Pi Coding Agent tool spans current during execution so nested spans attach to the tool span.
--- js/src/instrumentation/plugins/pi-coding-agent-plugin.test.ts
+++ js/src/instrumentation/plugins/pi-coding-agent-plugin.test.ts
@@ -27,6 +27,7 @@ vi.mock("../../isomorph", async (importOriginal) => {
 
 vi.mock("../../logger", () => ({
   startSpan: (...args: unknown[]) => mockStartSpan(...args),
+  withCurrent: (_span: unknown, callback: () => unknown) => callback(),
 }));
 
 import { isAutoInstrumentationSuppressed } from "../auto-instrumentation-suppression";
